448872b227fb96a66d4475a62c7fb526ec67621a
Mssuk <djqhr490@gmail.com>
2020-02-19 13:19:09 +0900

    수학공식 프로그램

--- default/Ex0219/src/app/Ex0219_01.java
+++ default/Ex0219/src/app/Ex0219_01.java
@@ -9,33 +9,40 @@ import java.util.Scanner;
 public class Ex0219_01 {
     public static void main(String[] args) {
         // 사칙 연산 프로그램
-
         Scanner scan = new Scanner(System.in);
-        int input1 = 0, input2 = 0;
+        int[] input = new int[2];
+        int result;
 
         while (true) {
             System.out.println(" [ 사칙연산 프로그램 ] ");
-            System.out.println("1.) 더하기 2.) 빼기");
+            System.out.println("1.) 더하기 2.) 빼기 3.) 곱하기 4.) 나누기");
             System.out.println(" 해당하는 프로그램을 선택하세요>>");
 
             switch (scan.nextInt()) {
                 case 1:
+                    print(input);
                     System.out.println("더하기 프로그램");
-                    System.out.println("더하고 싶은 첫 번째 숫자를 입력하세요.>>");
-                    input1 = scan.nextInt();
-                    System.out.println("더하고 싶은 두 번째 숫자를 입력하세요.>>");
-                    input2 = scan.nextInt();
-                    int result = Ex0219_01.add(input1, input2);
-                    System.out.println("결과값:" + result);
+                    add(input[0], input[1]);
                     break;
 
                 case 2:
+                    print(input);
                     System.out.println("빼기 프로그램");
-                    System.out.println("빼고 싶은 첫 번째 숫자를 입력하세요.>>");
-                    input1 = scan.nextInt();
-                    System.out.println("빼고 싶은 두 번째 숫자를 입력하세요.>>");
-                    input2 = scan.nextInt();
-                    System.out.println("결과값:" + sub(input1, input2));
+                    sub(input[0], input[1]);
+                    break;
+
+                case 3:
+                    print(input);
+                    System.out.println("곱하기 프로그램");
+
+                    result = mul(input[0], input[1]);
+                    System.out.println("결과값:" + result);
+                    break;
+
+                case 4:
+                    System.out.println("나누기 프로그램");
+
+                    Ex0219_01.div(input[0], input[1]);
                     break;
 
                 default:
@@ -44,19 +51,45 @@ public class Ex0219_01 {
         }
     } // main
 
+    // 프린트 메소드
+    static void print(int[] input) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("첫 번째 숫자를 입력하세요.>>");
+        input[0] = scan.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요.>>");
+        input[1] = scan.nextInt();
+
+    }
+
     // 더하기 method
-    static int add(int a, int b) {
+    static void add(int a, int b) {
         int result = 0;
         result = a + b;
         // 넘어갈 때 result변수가 넘어가지 않고 값이 넘어간다.
+
+        System.out.println("결과값:" + result);
+
+    }
+
+    static void sub(int a, int b) {
+        int result = 0;
+        if (a > b) {
+            result = a - b;
+        } else {
+            result = b - a;
+        }
+        System.out.println("결과값:" + result);
+    }
+
+    static int mul(int a, int b) {
+        int result = 0;
+        result = a * b;
         return result;
     }
 
-    static int sub(int a, int b) {
-        if (a > b) {
-            return a - b;
-        } else {
-            return b - a;
-        }
+    static void div(int a, int b) {
+        System.out.println("몫: " + a / b + ",  나머지: " + a % b);
     }
+
 } // class
---
+++ default/Ex0219/src/app/Ex0219_02.java
@@ -0,0 +1,142 @@
+package app;
+
+import java.util.Scanner;
+
+/**
+ * Ex0219_02
+ */
+public class Ex0219_02 {
+
+    public static void main(String[] args) {
+        // 1. 최대값 구하기 (3개의 숫자를 넣어서 1.최대값과 2.최대값이 얼마인지 최소값이 얼마인지를 알아내는 형태)
+        // 2. 절대값 구하기(1개의 숫자를 넣어서 절대값을 구하는 것)
+        // 3. 제곱값 구하기 (첫번째 숫자 5, 제곱 숫자 2)
+        // 1,2 원하는 번호를 입력하세요>>
+        Scanner scan = new Scanner(System.in);
+
+        loop1: while (true) {
+
+            print(); // 화면 출력 메소드
+
+            switch (scan.nextInt()) {
+                case 1:
+
+                    showMax();
+                    break;
+
+                case 2:
+                    showAbs();
+                    break;
+
+                case 3:
+                    showPow();
+                    break;
+
+                default:
+                    System.out.println("잘못된 입력");
+                    break;
+            }
+        }
+
+    } // main
+
+    static void print() {
+
+        System.out.println("---------------------------");
+        System.out.println("[ 수학공식 프로그램] ");
+        System.out.println("---------------------------");
+        System.out.println("1. 최대값 구하기 2. 절대값 구하기 3. 제곱값 구하기");
+        System.out.println("원하는 번호를 입력하세요...");
+
+    }
+
+    static void showMax() {
+
+        Scanner scan = new Scanner(System.in);
+        // 최대값 무한 반복
+        while (true) {
+
+            System.out.println("최대값 구하기..");
+            int[] arr = new int[3];
+            for_int(arr);
+            if (arr[0] == 99) {
+                break;
+            } else if (arr[0] == 0) {
+                break;
+            }
+
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+
+            // Math.max( )를 써도 된다.
+            // Math.max(Math.max(arr[0], arr[1]), arr[2])
+            for (int i = 0; i < arr.length; ++i) {
+                if (arr[i] > max) {
+                    max = arr[i];
+                }
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            System.out.println("최대값은 : " + max + " 최소값은 : " + min);
+        }
+
+    }
+
+    static void showAbs() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+
+            System.out.println("절대값 구하기..");
+            System.out.println("값을 입력하세요 ( 99: 이전화면 0: 종료)");
+            int selectNum = scan.nextInt();
+            if (selectNum == 99) {
+                break;
+            } else if (selectNum == 0) {
+                break;
+            }
+            int result = 0;
+            if (selectNum < 0) {
+                result = selectNum * -1;
+            } else {
+                result = selectNum;
+            }
+            System.out.println("절대값은 : " + result);
+        }
+    }
+
+    static void showPow() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+
+            System.out.println("제곱값 구하기..");
+            System.out.println("밑을 입력하세요 ( 99: 이전화면 0: 종료)");
+            int mit = scan.nextInt();
+            if (mit == 99) {
+                break;
+            } else if (mit == 0) {
+                break;
+            }
+            System.out.println("지수를 입력하세요");
+            int we = scan.nextInt();
+
+            System.out.println((int) Math.pow(mit, we));
+        }
+    }
+
+    static int[] for_int(int[] arr) {
+
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("숫자를 입력하세요. (이전화면: 99)>>>");
+            arr[i] = scan.nextInt();
+            if (arr[i] == 99) {
+                break;
+            } else if (arr[i] == 0) {
+                break;
+            }
+        }
+        return arr;
+    }
+
+} // calss
