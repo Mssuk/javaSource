f965e6ccc98fd6bf893123f907dd9171bebbecee
Mssuk <djqhr490@gmail.com>
2020-02-26 11:28:00 +0900

    super

--- default/Ex0226/src/app/Ex0226_02.java
+++ default/Ex0226/src/app/Ex0226_02.java
@@ -1,51 +1,62 @@
 package app;
 
+import java.util.Scanner;
+
 /**
  * Ex0226_02
  */
 public class Ex0226_02 {
 
+    Ex0226_02(){
+        super();
+    }
+
+    Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
         Point p = new Point(100, 100);
 
-        Point3D p3= new Point3D(50, 60, 70);
+        Point3D p3 = new Point3D(50, 60, 70);
         System.out.println(p3.getLocation());
 
-       System.out.println(p.getLocation());
+        System.out.println(p.getLocation());
     }
 }
 
-class Point{
+// 기본으로 Object를 상속함 (안 적어도)
+class Point extends Object {
     int x;
     int y;
 
-    // Point(){
-    //     this(0,0);
-    // }
-
-    Point(int x, int y){
-        this.x= x;
-        this.y= y;
+    Point() {
+        this(0, 0);
     }
 
-    String getLocation(){
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    String getLocation() {
         return String.format("X좌표: %d , Y좌표: %d", x, y);
     }
+    
 
 }
 
-class Point3D extends Point{
+class Point3D extends Point {
     int z;
 
-    Point3D(int x, int y, int z){
-        super(x, y);
-        this.x =x;
-        this.y= y;
-        this.z= z;
+    Point3D(int x, int y, int z) {
+        // Point 클래스에 기본 생성자를 안만들어주면 super(x,y) 로 직접 명시해야함.
+        // super(x, y);
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     @Override
-    String getLocation(){
+    String getLocation() {
         return super.getLocation() + ", Z좌표 : " + z;
     }
 
